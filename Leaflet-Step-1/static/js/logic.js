59337e04f66b402e18541d32fb3553c04b67054f
Ben Cliff <bocliffwm@gmail.com>
2020-05-05 12:26:34 -0400

    Got Part one of leaflet challenge completed

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,44 +1,54 @@
-
 //var jsonData = getJson("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson")
 
 // Create a map object
 var myMap = L.map("map", {
-    center: [33.31, -114.70],
-    zoom: 4
-    //access the data from var Events here
-  });
-  
-  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  center: [33.31, -114.7],
+  zoom: 2,
+  //access the data from var Events here
+});
+
+L.tileLayer(
+  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: "mapbox.streets-basic",
-    accessToken: API_KEY
-  }).addTo(myMap);
+    accessToken: API_KEY,
+  }
+).addTo(myMap);
 
-  L.CRS
+// Getting Json data
+var Events = [];
 
-  // Getting Json data
-var Events=[];
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson",function(data)
-{
+d3.json(
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson",
+  function (data) {
+    data.features.forEach(function (f) {
+      var loc = f.geometry.coordinates;
+      var x = loc[0];
+      var y = loc[1];
+      var z = loc[2];
+      var p = f.properties;
+      var magnitude = p.mag;
+      var place = p.place;
 
-    data.features.forEach(function (f){
-        var loc = f.geometry.coordinates;
-        var x = loc[0];
-        var y = loc[1];
-        var z = loc[2];
-        var p= f.properties;
-        var magnitude = p.mag;
-        var place = p.place;
+      var asDict = {
+        Long: x,
+        Lat: y,
+        Depth: z,
+        Mag: magnitude,
+        Place: place,
+      };
 
-        var asDict={
-            Long:x,
-            Lat:y,
-            Depth:z,
-            Mag:magnitude,
-            Place:place
-        };
+      Events.push(asDict);
+      var circle = L.circle([asDict.Lat, asDict.Long], {
+        color: "Red",
+        radius: 8000,
+      }).addTo(myMap);
+      circle.bindPopup(asDict.Place + asDict.Mag).openPopup();
+    });
+  });
 
-        Events.push(asDict);  
-    })
-});
+
+  
