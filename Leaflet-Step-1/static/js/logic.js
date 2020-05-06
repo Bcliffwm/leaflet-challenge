cd8c63b13436a1a6cf26ed1344c6c1c3ce873ca4
Ben Cliff <bocliffwm@gmail.com>
2020-05-06 13:58:28 -0400

    Replaced basic street map with grayscale map

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,23 +1,36 @@
 //var jsonData = getJson("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson")
-
 // Create a map object
 var myMap = L.map("map", {
   center: [33.31, -114.7],
-  zoom: 2,
+  zoom: 4,
   //access the data from var Events here
 });
 
+// var grayscale = L.tileLayer(
+//   "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+//   {
+//     attribution:
+//       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: "mapbox.streets-basic",
+//     accessToken: API_KEY,
+//   }
+// ).addTo(myMap);
 L.tileLayer(
   "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
   {
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: "mapbox.streets-basic",
+    id: "mapbox.light",
     accessToken: API_KEY,
   }
 ).addTo(myMap);
-
+// var baseMaps = {
+//   Grayscale: grayscale,
+//   Streets: street,
+// };
+// L.control.layers(baseMaps, circle).addTo(myMap);
 // Getting Json data
 var Events = [];
 
@@ -42,13 +55,34 @@ d3.json(
       };
 
       Events.push(asDict);
+      
+
+      // Create Switch for circle coloring and sizing
+      var color;
+
+      switch (asDict.Mag) {
+        case 1.2:
+          color = "Green";
+          break;
+        case 2.0:
+          color = "Blue";
+          break;
+        case 5.0:
+          color = "Black";
+          break;
+        case 4.0:
+          color = "Teal";
+          break;
+        default:
+          color = "Red";
+          break;
+      }
       var circle = L.circle([asDict.Lat, asDict.Long], {
-        color: "Red",
+        color: color,
         radius: 8000,
       }).addTo(myMap);
       circle.bindPopup(asDict.Place + asDict.Mag).openPopup();
+      
     });
-  });
-
-
-  
+  }
+);
